f5862bf8a7a3de0278a3cb6abafbd4853950b3b9
Aleksandra Litvinenko <alexandralit09022005@gmail.com>
2025-03-13 11:11:04 +0300

    Add Newton interpolation function

--- function_interpolation.py
+++ function_interpolation.py
@@ -15,14 +15,19 @@ def diff_x(function, arg) -> float:
     return lambdify(x, result_function)(arg)
 
 
-# Задаем интервал, на котором определена ф-ция
-interval = [5, 10]
+def make_net(interval: list, amount_of_nodes:int):
+    """Разбивает интервал на отрезки по количеству необходимых узлов"""
+    return np.linspace(interval[0], interval[1], amount_of_nodes)
 
-# Разбиваем отрезок на 3 части (4 точки)
-x_net = np.linspace(interval[0], interval[1], 4)
+
+# Задаем интервал, на котором определена ф-ция
+my_interval = [5, 10]
+
+# Разбиваем отрезок на 3 части (4 точки) для интерполяции по Лагранжу
+x_net_lagrangian = make_net(my_interval, 4)
 
 # Вычисляем значения на концах отрезка и в точках разбиения
-y_net = f(x_net)
+y_net_lagrangian = f(x_net_lagrangian)
 
 
 def lagrangian_interpolation(x_net, y_net, arg):
@@ -42,7 +47,35 @@ def lagrangian_interpolation(x_net, y_net, arg):
     return lagrange_function_result
 
 
-def plot_maker(x_net, y_net, function, interpolation_function):
+# Разбиваем отрезок на 7 частей(6 узлов) для интерполяции по Ньютону (deg = 5)
+x_net_newton = make_net(my_interval, 6)
+y_net_newton = f(x_net_newton)
+
+
+def newton_interpolation(x_net, y_net, arg):
+    """Интерполяция по Ньютону через разделенные разности"""
+    n = len(x_net)
+    x = symbols('x')
+
+    differences = np.zeros((n, n))
+    differences[:, 0] = y_net
+    for i in range(1, n):
+        for j in range(n - i):
+            differences[j, i] = (differences[j + 1, i - 1] - differences[j, i - 1]) / (x_net[j + i] - x_net[j])
+
+    coefficients_for_newton = differences[0, :]
+    newton_function = coefficients_for_newton[0]
+    polynom = 1
+    for i in range(1, len(coefficients_for_newton)):
+        polynom *= (x - x_net[i - 1])
+        newton_function += coefficients_for_newton[i] * polynom
+
+    newton_function_result = lambdify(x, newton_function)(arg)
+
+    return newton_function_result
+
+
+def plot_maker(x_net, y_net, function, interpolation_function, massage):
     x_values = np.linspace(x_net[0], x_net[-1], 50)
 
     interpolation_function_values = interpolation_function(x_net, y_net, x_values)
@@ -50,6 +83,7 @@ def plot_maker(x_net, y_net, function, interpolation_function):
     plt.plot(x_values, function(x_values), 'k-', label='Исходная функция f(x)')
     plt.plot(x_values, interpolation_function_values, 'r--', label='Интерполяция')
     plt.scatter(x_net, y_net, color='r', zorder=5, label='Узлы')
+    plt.title(massage)
     plt.xlabel('x')
     plt.ylabel('f(x)')
     plt.legend()
@@ -57,5 +91,6 @@ def plot_maker(x_net, y_net, function, interpolation_function):
     plt.show()
 
 
-plot_maker(x_net, y_net, f, lagrangian_interpolation)
+plot_maker(x_net_lagrangian, y_net_lagrangian, f, newton_interpolation, 'Интерполяция по Ньютону')
+plot_maker(x_net_lagrangian, y_net_lagrangian, f, lagrangian_interpolation, 'Интерполяция по Лагранжу')
 
